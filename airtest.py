6f3bc54a5ba9e132cbef55b8841c449791039f22
Will Dolan <willrdolan@gmail.com>
2025-08-03 12:57:39 -0500

    fig edits

--- airtest.py
+++ airtest.py
@@ -74,23 +74,34 @@ class PMSensor:
 			print('warn email sent')
 
 	def run(self, show_plot=False):
-		print('Running...')
-		self.animation = FuncAnimation(
-			self.fig,
-			self.update,
-			cache_frame_data=False,
-			interval=200
-		)
-		print('Blocking...')
-		
 		if show_plot:
+			self.animation = FuncAnimation(
+				self.fig,
+				self.update,
+				cache_frame_data=False,
+				interval=200
+			)
 			plt.show(block=True)
+		
 		else:
-			try:
-				while True:
-					time.sleep(1)
-			except KeyboardInterrupt:
-				print("Monitoring stopped")
+			while True:
+				pm25, pm10 = self.read_sensor()
+				
+				self.times.append(datetime.now())
+				self.readings25.append(pm25)
+				self.readings10.append(pm10)
+				
+				if (pm10 > 5 or pm25 > 3) and \
+				(time.time() - self.warn_delay > self.last_warn):
+					# Still update the plot for email attachment
+					self.line25.set_data(self.times[-30:], self.readings25[-30:])
+					self.line10.set_data(self.times[-30:], self.readings10[-30:])
+					self.fig.gca().relim()
+					self.fig.gca().autoscale_view()
+					
+					self.send_warn_email()
+					print('warn email sent')
+				time.sleep(0.2)
 
 def main():
 	try:
